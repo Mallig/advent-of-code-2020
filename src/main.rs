6bb3327d96eec5a78c7abf1ff53fc137e873a3f2
Malachy Gilchrist <malachygilchrist@gmail.com>
2020-12-01 18:16:52 +0000

    created day 1 imputs file and read it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+//use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+  // Day 1, just use main
+    let input = fs::read_to_string("/Users/malachygilchrist/Coding/Projects/Advent-Of-Code/advent_of_code_2020/inputs/day_1.txt")
+      .expect("Something went wrong reading the file");
+    
+    println!("{}", input);
 }
